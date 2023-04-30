d0c3889790abc1eca5667fdd49b90e95064a9ec6
Eric Gjertsen <ericgj72@gmail.com>
2023-04-30 14:32:58 -0400

    expose blockOfList; elm-format

--- src/Html/Bem.elm
+++ src/Html/Bem.elm
@@ -1,24 +1,13 @@
 module Html.Bem exposing
-    ( Block
-    , Element
-    , block
-    , blockIf
-    , blockList
-    , blockMod
-    , blockOf
-    , element
-    , elementIf
-    , elementList
-    , elementMod
-    , elementName
-    , elementNameMod
-    , elementNameOf
-    , elementOf
-    , elementOfList
-    , init
+    ( init
+    , block, element
+    , blockMod, elementMod, blockIf, elementIf, blockList, elementList
+    , blockOf, elementOf, blockOfList, elementOfList
+    , elementName, elementNameMod, elementNameOf
+    , Block, Element
     )
 
-{-| Utilities for building CSS class names based on 
+{-| Utilities for building CSS class names based on
 [BEM conventions](https://getbem.com/naming).
 
 
@@ -26,21 +15,26 @@ module Html.Bem exposing
 
 @docs init
 
+
 # Unmodified block and element classes
 
 @docs block, element
+
 
 # Flag modified block and element classes
 
 @docs blockMod, elementMod, blockIf, elementIf, blockList, elementList
 
+
 # Key-value modified block and element classes
 
 @docs blockOf, elementOf, blockOfList, elementOfList
 
+
 # Raw class strings
 
 @docs elementName, elementNameMod, elementNameOf
+
 
 # Types
 
@@ -52,12 +46,13 @@ import Html exposing (Attribute)
 import Html.Attributes exposing (class, classList)
 
 
-{-| Type for building block classes, and constructing elements of blocks. 
+{-| Type for building block classes, and constructing elements of blocks.
 -}
 type alias Block =
     { name : String
     , element : String -> Element
     }
+
 
 {-| Type for building element classes.
 -}
@@ -66,16 +61,18 @@ type alias Element =
     , name : String
     }
 
+
 {-| Construct a top-level [Block](#Block) with given name.
 
 Note an [Element](#Element) can only be constructed through the `.element` of
 a Block like this:
 
     let
-        block = 
+        block =
             Bem.init "my-block"
     in
     block.element "my-element"
+
 -}
 init : String -> Block
 init b =
@@ -83,23 +80,24 @@ init b =
     , element = Element b
     }
 
-{-| Element class string, e.g. "my-block__my-element"
+
+{-| Element class string, e.g. "my-block\_\_my-element"
 -}
 elementName : Element -> String
 elementName e =
     joinElement e.block e.name
 
 
-{-| Element class string with given flag modifier, e.g. 
-"my-block__my-element--flag"
+{-| Element class string with given flag modifier, e.g.
+"my-block\_\_my-element--flag"
 -}
 elementNameMod : Element -> String -> String
 elementNameMod e m =
     joinElementMod e.block e.name m
 
 
-{-| Element class string with given key-value modifier, e.g. 
-"my-block__my-element--key-value"
+{-| Element class string with given key-value modifier, e.g.
+"my-block\_\_my-element--key-value"
 -}
 elementNameOf : Element -> String -> String -> String
 elementNameOf e k v =
@@ -108,8 +106,9 @@ elementNameOf e k v =
 
 {-| Class attribute for unmodified block
 
-    Bem.init "my-block" |> Bem.block   
+    Bem.init "my-block" |> Bem.block
         -- class "my-block"
+
 -}
 block : Block -> Attribute a
 block b =
@@ -118,8 +117,9 @@ block b =
 
 {-| Class attribute for "key-value" modified block
 
-    Bem.init "my-block" |> Bem.blockOf "type" "foo"  
+    Bem.init "my-block" |> Bem.blockOf "type" "foo"
         -- class "my-block my-block--type-foo"
+
 -}
 blockOf : String -> String -> Block -> Attribute a
 blockOf k v b =
@@ -131,8 +131,9 @@ blockOf k v b =
 
 {-| Class attribute for "flag" modified block
 
-    Bem.init "my-block" |> Bem.blockMod "flagged"  
+    Bem.init "my-block" |> Bem.blockMod "flagged"
         -- class "my-block my-block--flagged"
+
 -}
 blockMod : String -> Block -> Attribute a
 blockMod m b =
@@ -141,22 +142,25 @@ blockMod m b =
 
 {-| Class attribute for "flag" modified block, based on given boolean flag
 
-    Bem.init "my-block" |> Bem.blockIf "flagged" isFlagged  
+    Bem.init "my-block" |> Bem.blockIf "flagged" isFlagged
         -- class "my-block my-block--flagged"  (if isFlagged is True)
         -- class "my-block"  (if isFlagged is False)
+
 -}
 blockIf : String -> Bool -> Block -> Attribute a
 blockIf m incl b =
-   classList 
-       [ ( b.name, True )
-       , ( joinBlockMod b.name m, incl )
-       ]
+    classList
+        [ ( b.name, True )
+        , ( joinBlockMod b.name m, incl )
+        ]
+
 
 {-| Class attribute for "flag" modified block, with multiple flags applied.
 Similar to Html.Attributes.classList for flag-modified blocks.
 
-    Bem.init "my-block" 
-        |> Bem.blockList [("flagged", isFlagged), ("editable", isEditable)]
+    Bem.init "my-block"
+        |> Bem.blockList [ ( "flagged", isFlagged ), ( "editable", isEditable ) ]
+
 -}
 blockList : List ( String, Bool ) -> Block -> Attribute a
 blockList list b =
@@ -168,8 +172,9 @@ blockList list b =
 {-| Class attribute for "key-value" modified block, with multiple key-value
 pairs applied.
 
-    Bem.init "my-block" 
-        |> Bem.blockOfList [( "type", "foo" ), ( "status", "open" ) ]   
+    Bem.init "my-block"
+        |> Bem.blockOfList [( "type", "foo" ), ( "status", "open" ) ]
+
 -}
 blockOfList : List ( String, String ) -> Block -> Attribute a
 blockOfList list b =
@@ -182,6 +187,7 @@ blockOfList list b =
 
     Bem.init "my-block" |> .element "my-element" |> Bem.element
         -- class "my-block__my-element"
+
 -}
 element : Element -> Attribute a
 element e =
@@ -190,8 +196,9 @@ element e =
 
 {-| Class attribute for "key-value" modified element
 
-    Bem.init "my-block" |> .element "my-element" |> Bem.elementOf "type" "foo"  
+    Bem.init "my-block" |> .element "my-element" |> Bem.elementOf "type" "foo"
         -- class "my-block__my-element my-block__my-element--type-foo"
+
 -}
 elementOf : String -> String -> Element -> Attribute a
 elementOf k v e =
@@ -203,8 +210,9 @@ elementOf k v e =
 
 {-| Class attribute for "flag" modified element
 
-    Bem.init "my-block" |> .element "my-element" |> Bem.elementMod "flagged"  
+    Bem.init "my-block" |> .element "my-element" |> Bem.elementMod "flagged"
         -- class "my-block__my-element my-block__my-element--flagged"
+
 -}
 elementMod : String -> Element -> Attribute a
 elementMod m e =
@@ -213,9 +221,10 @@ elementMod m e =
 
 {-| Class attribute for "flag" modified element, based on given boolean flag
 
-    Bem.init "my-block" |> .element "my-element" |> Bem.elementIf "flagged" isFlagged  
+    Bem.init "my-block" |> .element "my-element" |> Bem.elementIf "flagged" isFlagged
         -- class "my-block__my-element my-block__my-element--flagged"  (if isFlagged is True)
         -- class "my-block__my-element"  (if isFlagged is False)
+
 -}
 elementIf : String -> Bool -> Element -> Attribute a
 elementIf m incl e =
@@ -228,9 +237,10 @@ elementIf m incl e =
 {-| Class attribute for "flag" modified element, with multiple flags applied.
 Similar to Html.Attributes.classList for flag-modified elements.
 
-    Bem.init "my-block" 
+    Bem.init "my-block"
         |> .element "my-element"
-        |> Bem.elementList [("flagged", isFlagged), ("editable", isEditable)]
+        |> Bem.elementList [ ( "flagged", isFlagged ), ( "editable", isEditable ) ]
+
 -}
 elementList : List ( String, Bool ) -> Element -> Attribute a
 elementList list e =
@@ -242,9 +252,10 @@ elementList list e =
 {-| Class attribute for "key-value" modified element, with multiple key-value
 pairs applied.
 
-    Bem.init "my-block" 
-        |> .element "my-element" 
-        |> Bem.elementOfList [( "type", "foo" ), ( "status", "open" ) ]   
+    Bem.init "my-block"
+        |> .element "my-element"
+        |> Bem.elementOfList [( "type", "foo" ), ( "status", "open" ) ]
+
 -}
 elementOfList : List ( String, String ) -> Element -> Attribute a
 elementOfList list e =
